54654ce947c59711fd478bb6a1299daa67c76dc3
First_Parimate <Parimatefirst@gmail.com>
2023-12-22 00:25:12 +0700

    update Display

--- client/src/pages/components/Display.js
+++ client/src/pages/components/Display.js
@@ -1,86 +1,168 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./Display.css";
+import Table from 'react-bootstrap/Table';
 
 const Display = ({ contract, account }) => {
   // กำหนด state สำหรับเก็บข้อมูลภาพที่ได้รับจาก contract
   const [data, setData] = useState("");
+  const [sharedData, setSharedData] = useState([]);
+  const [showSharedData, setShowSharedData] = useState(false);
 
-  // ฟังก์ชัน getdata เพื่อดึงข้อมูลภาพจาก contract
+  // ฟังก์ชันเพื่อดึงข้อมูลภาพจาก contract
   const getdata = async () => {
+    let dataArray;
     // ดึงค่าที่อยู่ที่ต้องการดึงภาพ
     const Otheraddress = document.querySelector(".address").value;
-    let dataArray;
 
     try {
       if (Otheraddress) {
         // เรียกใช้งานฟังก์ชัน display ใน contract และส่งที่อยู่ที่ต้องการดึงภาพเข้าไป
         dataArray = await contract.display(Otheraddress);
-        console.log(dataArray);
       } else {
         // ถ้าไม่ได้ใส่ที่อยู่ ให้ดึงภาพของบัญชีปัจจุบัน (account)
         dataArray = await contract.display(account);
       }
     } catch (e) {
-      alert("You don't have access"); // แสดงข้อความแจ้งเตือนในกรณีที่ไม่สามารถดึงภาพได้
+      // แสดงข้อความแจ้งเตือนในกรณีที่ไม่สามารถดึงภาพได้
+      alert("You don't have access");
+      return;
     }
 
+    //การตรวจสอบว่า dataArray นั้นว่างเปล่าหรือไม่
     const isEmpty = Object.keys(dataArray).length === 0;
 
-    if (!isEmpty) { // ตรวจสอบว่ามีข้อมูลภาพหรือไม่
+    // ถ้า dataArray ไม่ว่างเปล่า
+    if (!isEmpty) {
+      // แปลง `dataArray` เป็น string
       const str = dataArray.toString();
-      const str_array = str.split(",");
-      
-      
-      // // สร้างอาร์เรย์ของภาพที่ดึงมาจาก contract เพื่อแสดงผลทีละภาพ
-      // const images = str_array.map((item, i) => {
-
-        // สร้างอาร์เรย์ของภาพที่ดึงมาจาก contract เพื่อแสดงผลทีละภาพ
-      const images = str_array.map((data, i) => {
+      // แบ่ง string ที่ได้เป็น array โดยใช้ ";" เป็นตัวแบ่ง
+      const str_array = str.split(";");
+      // สร้างอาร์เรย์ของภาพที่ดึงมาจาก contract เพื่อแสดงผลทีละภาพ
+      const images = str_array.map((item, i) => {
+        // แยกข้อมูลจาก item แล้วแสดงผล
+        //const [list, firstName, lastName, studentId, faculty, department, certificateName] = item.split(",");
+        const splitItems = item.split(",");
+        const certificateName = splitItems.pop();
+        const department = splitItems.pop();
+        const faculty = splitItems.pop();
+        const studentId = splitItems.pop();
+        const lastName = splitItems.pop();
+        const firstName = splitItems.pop();
+        const list = splitItems.join(","); // คำสั่ง join ใช้เพื่อรวมข้อมูลที่เหลือกันให้เป็น string และใช้ตัวคั่นเป็น ","
+                
+        
         return (
-          
           <div key={i} className="image-container">
-             <div className="image-info">
-              {/* Display additional information */}
-              <p>Full Name: {data.fullName}</p>
-              <p>Student ID: {data.studentId}</p>
-              <p>Faculty: {data.faculty}</p>
-              <p>Department: {data.department}</p>
-              <p>Certificate Name: {data.certificateName}</p>
-              
+            <div className="image-info">
+              <p>list {list}</p>
+              <p>First Name: {firstName}</p>
+              <p>Last Name: {lastName}</p>
+              <p>Student ID: {studentId}</p>
+              <p>Faculty: {faculty}</p>
+              <p>Department: {department}</p>
+              <p>Certificate Name: {certificateName}</p>
             </div>
-
-          <a href={data} key={i} target="_blank" rel="noreferrer">
-            <img
-              key={i}
-              src={`https://gateway.pinata.cloud/ipfs/${data.substring(6)}`}
-              alt="Click Link"
-            ></img>
-          </a>
-          <br/>
+            <a href={item} key={i} target="_blank" rel="noreferrer">
+              <img
+                key={i}
+                src={`https://gateway.pinata.cloud/ipfs/${item.substring(6)}`}
+                alt="Click Link"
+                className="image-list"
+              ></img>
+            </a>
+            <br />
           </div>
         );
       });
       setData(images); // อัปเดต state data เพื่อแสดงภาพที่ดึงมาจาก contract
     } else {
-      alert("No image to display"); // แสดงข้อความแจ้งเตือนในกรณีที่ไม่มีภาพที่ต้องการแสดง
+      // แสดงข้อความแจ้งเตือนในกรณีที่ไม่มีภาพที่ต้องการแสดง
+      alert("No image to display");
     }
   };
 
-  // JSX ส่วนที่แสดงผลบนหน้าเว็บ
+  // ฟังก์ชันสำหรับการดึงข้อมูลที่ได้รับการแชร์ผ่าน contract
+  const getSharedData = useCallback(async () => {
+    try {
+      // เรียกใช้งานฟังก์ชัน shareAccess จาก contract เพื่อดึงข้อมูล
+      const result = await contract.shareAccess();
+
+      // อัปเดตข้อมูลที่ได้รับจาก contract ไปยัง state ของ component
+      setSharedData(result);
+    } catch (e) {
+      // แสดงข้อความแจ้งเตือนในกรณีที่เกิดข้อผิดพลาด
+      //alert("Error fetching shared data");
+    }
+  }, [contract]);
+
+  // เมื่อ component ถูกโหลดหรือ state ที่เกี่ยวข้องมีการเปลี่ยนแปลง
+  // ให้เรียกใช้งานฟังก์ชัน getSharedData เพื่อดึงข้อมูลที่ได้รับการแชร์ใหม่
+  useEffect(() => {
+    getSharedData();
+  }, [getSharedData]);
+
+
   return (
     <>
       <h1 style={{ color: "black" }}>Certificate list</h1>
+      <p>Current Timestamp: { }</p>
       <div className="image-list">{data}</div>
-      {/* ตัวอินพุตสำหรับกรอกที่อยู่ที่ต้องการดึงภาพ */}
       <input
         type="text"
         placeholder="Enter Address"
         className="address"
       ></input>
-      {/* ปุ่มที่ให้คลิกเพื่อดึงภาพ */}
-      <button className="center button" onClick={getdata}>
-        Get Data
+      <button className="center button" onClick={getdata}>Get Data</button><br />
+
+      {/* ปุ่มเพิ่มเติมสำหรับแสดง/ซ่อนข้อมูลที่ได้จาก shareAccess */}
+      <button className="shared" onClick={() => setShowSharedData(!showSharedData)}>
+        Shared Access Data
       </button>
+      <br />
+      <br />
+
+      {/* แสดงข้อมูลที่ได้จาก shareAccess เมื่อคลิกปุ่ม */}
+      {showSharedData && (
+        <div className="shared-data">
+          <h2>Shared Access Data</h2><br />
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Student ID</th>
+                <th>Faculty</th>
+                <th>Department</th>
+                <th>Certificate Name</th>
+                <th>Address User</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sharedData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastNameName}</td>
+                  <td>{item.studentId}</td>
+                  <td>{item.faculty}</td>
+                  <td>{item.department}</td>
+                  <td>{item.certificateName}</td>
+                  <td>{item.user}</td>
+                  <td>
+                    {item.fileHash ? (
+                      <a href={`https://gateway.pinata.cloud/ipfs/${item.fileHash}`} target="_blank" rel="noreferrer">
+                        View Image
+                      </a>
+                    ) : (
+                      "No Image Link"
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+      )}
     </>
   );
 };
--- client/src/pages/components/FileUpload.js
+++ client/src/pages/components/FileUpload.js
@@ -9,7 +9,7 @@ import Row from 'react-bootstrap/Row';
 
 
 // Component ชื่อ FileUpload รับ props 3 ตัว contract, account, provider
-const FileUpload = ({ contract, account, provider }) => {
+const FileUpload = ({ contract, account}) => {
   // สร้าง state 2 ตัวคือ file และ fileName โดยให้เริ่มต้นค่าเป็น null และ "No image selected" ตามลำดับ
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("No image selected");
@@ -41,28 +41,27 @@ const FileUpload = ({ contract, account, provider }) => {
         const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
 
         // Collect additional data from the form
-        //const endTime = 1000000000000000;
-        const fullName = document.getElementById("validationCustom01").value;
+        const firstName = document.getElementById("validationCustom01").value;
+        const lastName = document.getElementById("validationCustom02").value;
         const studentId = document.getElementById("validationCustomID").value;
-        const certificateName = document.getElementById("validationCustom03").value;
-        const faculty = document.getElementById("validationCustom04").value;
-        const department = document.getElementById("validationCustom05").value;
-        const gasLimit = 5000000; // Set an appropriate gas limit
+        const faculty = document.getElementById("validationCustom03").value;
+        const department = document.getElementById("validationCustom04").value;
+        const certificateName = document.getElementById("validationCustom05").value;
+        //const gasLimit = 500000; // Set an appropriate gas limit
 
-
-        await contract.add(account, ImgHash , 0, fullName , studentId, certificateName, faculty, department, {gasLimit}); // เรียกใช้ function add ในสัญญาอัจฉริยะโดยให้พารามิเตอร์ account และ ImgHash
+        
+        // เรียกใช้ function add ในสัญญาอัจฉริยะโดยให้พารามิเตอร์ account และ ImgHash
+        // await contract.add(account, ImgHash);
+        
+        await contract.add(firstName, lastName, studentId, faculty, department, certificateName, account, 0, ImgHash); 
         alert("Successfully Image Uploaded"); // แสดงข้อความแจ้งเตือนว่าอัปโหลดภาพสำเร็จ
         setFileName("No image selected"); // รีเซ็ตชื่อไฟล์ที่เลือกให้เป็น "No image selected"
-        setFile(null); // รีเซ็ต state file เป็น null เพื่อให้สามารถเลือกภาพใหม่ได้
+        setFile(null); // รีเซ็ต state file เป็น null เพื่อให้สามารถเลือกภาพใหม่ได    
       } catch (e) {
+        console.error("Error uploading image to Pinata:", e);
         alert("Unable to upload image to Pinata"); // แสดงข้อความแจ้งเตือนว่าไม่สามารถอัปโหลดภาพไปยัง Pinata ได้
       }
-    }
-    alert("Successfully Image Uploaded"); // แสดงข้อความแจ้งเตือนว่าอัปโหลดภาพสำเร็จ (บรรทัดนี้อาจซ้ำกัน)
-    setFileName("No image selected"); // รีเซ็ตชื่อไฟล์ที่เลือกให้เป็น "No image selected" (บรรทัดนี้อาจซ้ำกัน)
-    setFile(null); // รีเซ็ต state file เป็น null เพื่อให้สามารถเลือกภาพใหม่ได้ (บรรทัดนี้อาจซ้ำกัน)
-    setValidated(true);
-    
+    }  
   };
 
   // function ชื่อ retrieveFile ทำการดึงข้อมูลจากไฟล์ที่ผู้ใช้เลือกและอัปเดต state file และ fileName
@@ -86,7 +85,7 @@ const FileUpload = ({ contract, account, provider }) => {
     setValidated(true);
   };
 
-  // JSX ส่วนที่แสดงผลบนหน้าจอ
+
   return (
     <div className="top">
       <h1 style={{ color: "black" }}>Certificate Upload</h1>
@@ -101,7 +100,7 @@ const FileUpload = ({ contract, account, provider }) => {
               placeholder="First name"
               defaultValue="Parimate"
             />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback></Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="4" controlId="validationCustom02">
             <Form.Label>Last name</Form.Label>
@@ -111,7 +110,7 @@ const FileUpload = ({ contract, account, provider }) => {
               placeholder="Last name"
               defaultValue="Jaroensong"
             />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback></Form.Control.Feedback>
           </Form.Group>
           <Form.Group as={Col} md="4" controlId="validationCustomID">
             <Form.Label>Student ID</Form.Label>
@@ -131,25 +130,25 @@ const FileUpload = ({ contract, account, provider }) => {
           </Form.Group>
         </Row>
         <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom03">
-            <Form.Label>Certificate Name</Form.Label>
-            <Form.Control type="text" placeholder="Certificate Name" required defaultValue="Basic Programing" />
-            <Form.Control.Feedback type="invalid">
-              Please provide a valid Certificate Name.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group as={Col} md="3" controlId="validationCustom04">
+          <Form.Group as={Col} md="3" controlId="validationCustom03">
             <Form.Label>Faculty</Form.Label>
             <Form.Control type="text" placeholder="Faculty" required defaultValue="Engineering"/>
             <Form.Control.Feedback type="invalid">
               Please provide a valid Faculty.
             </Form.Control.Feedback>
           </Form.Group>
-          <Form.Group as={Col} md="3" controlId="validationCustom05">
+          <Form.Group as={Col} md="3" controlId="validationCustom04">
             <Form.Label>Department</Form.Label>
             <Form.Control type="text" placeholder="Department" required defaultValue="Computer Engineering"/>
             <Form.Control.Feedback type="invalid">
               Please provide a valid Department.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="validationCustom05">
+            <Form.Label>Certificate Name</Form.Label>
+            <Form.Control type="text" placeholder="Certificate Name" required defaultValue="Basic Programing" />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid Certificate Name.
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
